8b482a7571fe35b338b1f7732ca4de9d52d539c1
Jon Shier <jon@jonshier.com>
2026-03-29 14:56:08 -0400

    Firewalk Release

--- Formula/firewalk.rb
+++ Formula/firewalk.rb
@@ -2,8 +2,8 @@ class Firewalk < Formula
   desc "Firewalk HTTP Test Server"
   homepage "https://github.com/Alamofire/Firewalk"
 
-  url "https://github.com/Alamofire/Firewalk/archive/refs/tags/0.12.1.tar.gz", :using => :curl
-  sha256 "063af6883e820c2570805824cecb9a40644e5dfe78f16fee9a2ca2d8721a82a8"
+  url "https://github.com/Alamofire/Firewalk/releases/download/0.12.2/firewalk.zip", :using => :curl
+  sha256 "49c2eed387e4f874a57c2a87e899494d55712eedf98a5f833494f5c7670095a2"
 
   def install
     bin.install "firewalk"
